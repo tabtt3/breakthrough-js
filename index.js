2fe26d69d7729cf3a7eebbb519b15035e53ce0a4
tabtt3 <tabttppp@gmail.com>
2020-03-27 00:18:09 +0900

    add validation

---
+++ index.js
@@ -0,0 +1,55 @@
+function AppModel() {
+  this.val = ""
+
+  this.attrs = {
+    required: "",
+    maxlength: 8,
+    minlength: 4
+  }
+
+  this.listeners = {
+    valid: [],
+    invalid: []
+  }
+}
+
+AppModel.prototype.on = function(event, func) {
+  this.listeners[event].push(func)
+}
+
+AppModel.prototype.trigger = function(event) {
+  $.each(this.listeners[event], function() {
+    this()
+  })
+}
+
+AppModel.prototype.set = function(val) {
+  if(this.val === val) return;
+  this.val = val
+  this.validate()
+}
+
+AppModel.prototype.required = function() {
+  return this.val !== ""
+}
+
+AppModel.prototype.maxlength = function(num) {
+  return num >= this.val.length
+}
+
+AppModel.prototype.minlength = function(num) {
+  return num <= this.val.length
+}
+
+AppModel.prototype.validate = function() {
+  var val
+  this.errors = []
+
+  for  (var key in this.attrs) {
+    val = this.attrs[key]
+    if ( !this[key](val) ) this.errors.push(key)
+  }
+
+  this.trigger( !this.errors.length ? "valid" : "invalid" )
+}
+
